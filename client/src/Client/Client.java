66f731b004308db2d5b2f7d5f341620587267ac0
crazytan <tjtanjia.tan@gmail.com>
2015-11-16 00:56:43 -0500

    add masterKey generation

--- client/src/Client/Client.java
+++ client/src/Client/Client.java
@@ -1,9 +1,33 @@
 package Client;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+
 /*
  * A class representing the client side of s2dr service.
  */
 public class Client {
+
+    private UID name;
+
+    private SecretKey masterKey;
+
+    public UID getName() { return name; }
+
+    public Client(String name) {
+        this.name = new UID(name);
+
+        // generate master key
+        try {
+            KeyGenerator gen = KeyGenerator.getInstance("AES");
+            masterKey = gen.generateKey();
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void init_session(String hostname) {}
 
     public void check_out(UID document_id) {}
@@ -18,4 +42,8 @@ public class Client {
     public void terminate() {}
 
     public static Client All;
+
+    public static void main(String[] args) {
+        Client c = new Client("tan");
+    }
 }
