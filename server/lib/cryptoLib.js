9eaa81f2d5a7024a585d47ecf1393c85dab66f86
crazytan <tjtanjia.tan@gmail.com>
2015-11-19 21:55:38 -0500

    add TODOs

--- server/lib/cryptoLib.js
+++ server/lib/cryptoLib.js
@@ -3,25 +3,25 @@
  */
 
 exports.decryptAES = function (m, key) {
-
+    // TODO
 };
 
 exports.encryptAES = function (m, key) {
-
+// TODO
 };
 
 exports.decryptKey = function (encryptedKey) {
-
+// TODO
 };
 
 exports.encryptKey = function (key) {
-
+// TODO
 };
 
 exports.hash = function (m) {
-
+// TODO
 };
 
 exports.generateAESKey = function () {
-
+// TODO
 };
--- server/routes/init.js
+++ server/routes/init.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
+    // TODO: add three phase init
 });
 
 module.exports = router;
