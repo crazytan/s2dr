cce630f13936550490585cf0b4c118598a5d42df
crazytan <tjtanjia.tan@gmail.com>
2015-11-21 12:21:15 -0500

    complete decrypt/encrypt AES

--- server/lib/cryptoLib.js
+++ server/lib/cryptoLib.js
@@ -1,13 +1,24 @@
 /*
  * a cryptography module
  */
+var crypto = require('crypto'),
+    aesKeyLen = 256, // # of bits of AES key
+    aesAlgorithm = 'aes-256-ctr';
 
+// return decrypted string in HEX
 exports.decryptAES = function (m, key) {
-    // TODO
+    var cipher = crypto.createCipher(aesAlgorithm, key.toString('binary'));
+    var decrypted = cipher.update(m.toString('hex'), 'hex', 'hex');
+    decrypted += cipher.final('hex');
+    return decrypted;
 };
 
+// return encrypted string in HEX
 exports.encryptAES = function (m, key) {
-// TODO
+    var cipher = crypto.createCipher(aesAlgorithm, key.toString('binary'));
+    var encrypted = cipher.update(m.toString('hex'), 'hex', 'hex');
+    encrypted += cipher.final('hex');
+    return encrypted;
 };
 
 exports.decryptKey = function (encryptedKey) {
@@ -23,5 +34,6 @@ exports.hash = function (m) {
 };
 
 exports.generateAESKey = function () {
-// TODO
+    const key = crypto.randomBytes(aesKeyLen / 8);
+    key.toString('hex');
 };
