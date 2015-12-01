77d9cd1eafc4a0936ce872802bf4950b1dcd6b1c
crazytan <tjtanjia.tan@gmail.com>
2015-11-30 23:15:20 -0500

    add ProcessUtil

--- client/src/CA/CA.java
+++ client/src/CA/CA.java
@@ -1,8 +1,6 @@
 package CA;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * A class representing Certificate Authority
@@ -11,28 +9,17 @@ public class CA {
 
     private static final File root = new File(System.getenv("root"));
 
-    public static byte[] extractPublicKeyFromCertificate(String certificate) {
+    public static String extractPublicKeyFromCertificate(String certificate) {
+
         return null;
     }
 
     public static boolean validateCertificate(String certificate) {
         try {
-            ProcessBuilder pb = new ProcessBuilder("openssl", "verify", "-trusted", "CA.crt");
-            pb.directory(root);
-            Process ps = pb.start();
-
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(ps.getOutputStream()));
-            writer.write(certificate);
-            writer.close();
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(ps.getInputStream()));
-            StringBuilder sb = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-                sb.append('\n');
-            }
-            if (sb.indexOf("OK") != -1) return true;
+            Process ps = ProcessUtil.createOpenSSLProcess(new String[] {"verify", "-trusted", "CA.crt"});
+            ProcessUtil.writeStandardInput(certificate, ps);
+            String result = ProcessUtil.getStandardOutput(ps);
+            if (result.contains("OK")) return true;
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -48,40 +35,11 @@ public class CA {
             writer.write(privateKey);
             writer.close();
 
-            ProcessBuilder pb = new ProcessBuilder("openssl", "req", "-new",
-                    "-key", tmpKey.getName(),
-                    "-subj", "/CN=" + name);
-            pb.directory(root);
-            Process ps = pb.start();
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(ps.getInputStream()));
-            StringBuilder sb = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-                sb.append('\n');
-            }
-            String csr = sb.toString();
-
-            pb = new ProcessBuilder("openssl", "x509", "-req",
-                    "-days", "365",
-                    "-CA", "CA.crt",
-                    "-CAkey", "CA.key",
-                    "-CAserial", "CA.srl");
-            pb.directory(root);
-            ps = pb.start();
-
-            writer = new BufferedWriter(new OutputStreamWriter(ps.getOutputStream()));
-            writer.write(csr);
-            writer.close();
-
-            reader = new BufferedReader(new InputStreamReader(ps.getInputStream()));
-            sb = new StringBuilder();
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-                sb.append('\n');
-            }
-            return sb.toString();
+            Process ps = ProcessUtil.createOpenSSLProcess(new String[] {"req", "-new", "-key", tmpKey.getName(), "-subj", name});
+            String csr = ProcessUtil.getStandardOutput(ps);
+            ps = ProcessUtil.createOpenSSLProcess(new String[] {"x509", "-req", "-days", "365", "-CA", "CA.crt", "-CAkey", "CA.key", "-CAserial", "CA.srl"});
+            ProcessUtil.writeStandardInput(csr, ps);
+            return ProcessUtil.getStandardOutput(ps);
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -93,21 +51,6 @@ public class CA {
     }
 
     public static void main(String[] args) throws IOException {
-        System.out.println(validateCertificate(new String("-----BEGIN CERTIFICATE-----\n" +
-                "MIICjzCCAXcCAQUwDQYJKoZIhvcNAQEFBQAwDTELMAkGA1UEAxMCQ0EwHhcNMTUx\n" +
-                "MjAxMDMzNzAzWhcNMTYxMTMwMDMzNzAzWjAOMQwwCgYDVQQDEwN0YW4wggEiMA0G\n" +
-                "CSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC+5qPFRLGyo4/pUhlBswOWjsFKfmDy\n" +
-                "8VOcVxwRBZ/zeZNWcx92s62HKct1ObNHX7PqD+uG7lOliyTCRuQSf3fdz3QALOfb\n" +
-                "z97073TjipCgJo/kZZK644E2Qb4a1URbDKOpzoWReBjy20tDthpVe1LQygFVZdlJ\n" +
-                "GTPWmPr56aACsJQpy8XGVyIj3y5VvKQylN6EtTpsGp50jjeZt1WpXYKvYDsTeRuR\n" +
-                "Y3nxrVXxX8Ju1sXljDMMHn5DBmWnfC3elZVSsmHBsdoPHvidX4SMwJlj6fqqDcGO\n" +
-                "st+MokCVvdrK0ZmM/fCYVZX82Sws23nXEVVEwPOmEWyL0Dk7NhCwH77jAgMBAAEw\n" +
-                "DQYJKoZIhvcNAQEFBQADggEBAL7D/LeW3jg2Vqghjd4qHcfaWLTZ+gkJSNOedlQT\n" +
-                "vYnJgicASqrU4ajcjpQG09gwi281LPEpOF3O4h6H6YSA6fQY4PvFAyRyfKNushnU\n" +
-                "tL8+ercHTLInwkf1amN3KGl44C06uWkwclKnVd8jAi90Utl1+vCqq3O2UXnncRxy\n" +
-                "dcKH/p0FU5EzmVXB7GZAU3TIQ1rDsssMjOAG0uV3WYu3XHmRULkkU8vWvW8x5adc\n" +
-                "b0ZsRQtyqYG7XDestEM4gMeJZoddARoYCC4qHBEP7GE4i4bOREntvjz1OF3+aUQv\n" +
-                "MmxlGIbpumVLsZ0TKfwJuXr2vvDmico/vIlSm7lfbclLU5w=\n" +
-                "-----END CERTIFICATE-----\n")));
+
     }
 }
---
+++ client/src/CA/ProcessUtil.java
@@ -0,0 +1,51 @@
+package CA;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A utility class for process management
+ */
+public class ProcessUtil {
+
+    private static final File root = new File(System.getenv("root"));
+
+    public static Process createOpenSSLProcess(String[] args) throws IOException {
+        List<String> command = new ArrayList<>();
+        command.add("openssl");
+        command.addAll(Arrays.asList(args));
+        ProcessBuilder pb = new ProcessBuilder(command);
+        pb.directory(root);
+        return pb.start();
+    }
+
+    public static void writeStandardInput(String in, Process ps) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(ps.getOutputStream()));
+        writer.write(in);
+        writer.close();
+    }
+
+    public static String getStandardOutput(Process ps) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(ps.getInputStream()));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+            sb.append('\n');
+        }
+        return sb.toString();
+    }
+
+    public static String getStandardError(Process ps) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(ps.getErrorStream()));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+            sb.append('\n');
+        }
+        return sb.toString();
+    }
+}
