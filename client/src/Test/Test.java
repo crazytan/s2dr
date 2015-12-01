1d89fcbbb5792d9e0b33f4c55725ac7603b0501d
crazytan <tjtanjia.tan@gmail.com>
2015-12-01 01:42:57 -0500

    complete Test

--- client/src/Test/Test.java
+++ client/src/Test/Test.java
@@ -3,10 +3,13 @@ package Test;
 import Client.ClientCrypto;
 import Client.InsecureMessage;
 import Client.SecureClient;
+import Client.SecureClient.SecurityFlag;
+import Client.SecureClient.Permission;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Test script
@@ -19,6 +22,15 @@ public class Test {
 
     private static SecureClient client1;
 
+    private static SecureClient client2;
+
+    private static void saveFile(String client, String fileName, String text) throws IOException {
+        File file = new File(workspace.getAbsolutePath() + "/" + client + "/" + fileName);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        writer.write(text);
+        writer.close();
+    }
+
     private static InsecureMessage getSuccessMessage(InsecureMessage message) {
         if (!message.isSuccess()) {
             System.out.println("failed! Message: " + message.getMessage());
@@ -30,7 +42,7 @@ public class Test {
 
     private static InsecureMessage getErrorMessage(InsecureMessage message) {
         if (message.isSuccess()) {
-            System.out.println("failed!");
+            System.out.println("test failed!");
             System.exit(1);
         }
         System.out.println(message.getMessage());
@@ -45,6 +57,10 @@ public class Test {
         System.out.print("Creating client_1...");
         client1 = new SecureClient("client_1");
         System.out.println("done!");
+
+        System.out.print("Creating client_2...");
+        client2 = new SecureClient("client_2");
+        System.out.println("done!");
     }
 
     public static void main(String[] args) throws Exception {
@@ -55,7 +71,7 @@ public class Test {
 
         System.out.print("Client0: checking in 0.txt...");
         String text = "hello world";
-        getSuccessMessage(client0.check_in(client0.generateUID("0.txt"), text, SecureClient.SecurityFlag.integrity));
+        getSuccessMessage(client0.check_in(client0.generateUID("0.txt"), text, SecurityFlag.integrity));
 
         System.out.println("Checking signature...");
         String signature = ClientCrypto.toHexString(ClientCrypto.doSHA256(text.getBytes()));
@@ -70,10 +86,8 @@ public class Test {
 
         System.out.print("Client0: checking out 0.txt...");
         text = getSuccessMessage(client0.check_out(client0.generateUID("0.txt"))).getMessage();
-        File copy = new File(workspace.getAbsolutePath() + "/client_0/0_copy.txt");
-        BufferedWriter writer = new BufferedWriter(new FileWriter(copy));
-        writer.write(text);
-        writer.close();
+        System.out.println("message: " + text);
+        saveFile("client_0", "0_copy.txt", text);
 
         System.out.print("Client1: initializing session with server...");
         getSuccessMessage(client1.init_session("server"));
@@ -86,5 +100,81 @@ public class Test {
 
         System.out.print("Client0: checking out 0.txt...");
         getErrorMessage(client0.check_out(client0.generateUID("0.txt")));
+
+        System.out.print("Client0: checking in 1.txt...");
+        getSuccessMessage(client0.check_in(client0.generateUID("1.txt"), text, SecurityFlag.confidentiality));
+
+        System.out.print("Client0: checking out 1.txt...");
+        text = getSuccessMessage(client0.check_out(client0.generateUID("1.txt"))).getMessage();
+        System.out.println("message: " + text);
+        saveFile("client_0", "1_copy.txt", text);
+
+        System.out.print("Client0: terminating session...");
+        getSuccessMessage(client0.terminate());
+
+        System.out.print("Client0: initializing session with server...");
+        getSuccessMessage(client0.init_session("server"));
+
+        System.out.print("Client0: checking in 1.txt...");
+        getSuccessMessage(client0.check_in(client0.generateUID("1.txt"), text, SecurityFlag.both));
+
+        System.out.print("Client0: checking out 1.txt...");
+        text = getSuccessMessage(client0.check_out(client0.generateUID("1.txt"))).getMessage();
+        System.out.println("message: " + text);
+        saveFile("client_0", "1_copy2.txt", text);
+
+        System.out.print("Client0: delegating to client1...");
+        getSuccessMessage(client0.delegate(client0.generateUID("1.txt"), client1, 30, Permission.both, false));
+
+        System.out.print("Client1: checking out 1.txt...");
+        text = getSuccessMessage(client1.check_out(client1.generateUID("1.txt"))).getMessage();
+        System.out.println("message: " + text);
+        saveFile("client_1", "1_copy.txt", text);
+
+        System.out.print("Client1: checking in 1.txt...");
+        getSuccessMessage(client1.check_in(client1.generateUID("1.txt"), "all work and no play makes Jack a dull boy", SecurityFlag.both));
+
+        System.out.print("Client1: delegating to client2...");
+        getErrorMessage(client1.delegate(client1.generateUID("1.txt"), client2, 30, Permission.both, false));
+
+        System.out.print("Client2: initializing session with server...");
+        getSuccessMessage(client2.init_session("server"));
+
+        System.out.print("Client2: checking out 1.txt...");
+        getErrorMessage(client2.check_out(client2.generateUID("1.txt")));
+
+        System.out.println("sleep 30 seconds...");
+        Thread.sleep(30000);
+        System.out.print("Client1: checking out 1.txt...");
+        getErrorMessage(client1.check_out(client1.generateUID("1.txt")));
+
+        System.out.print("Client0: checking out 1.txt...");
+        getSuccessMessage(client0.check_out(client0.generateUID("1.txt")));
+
+        System.out.print("Client0: delegating to client1...");
+        getSuccessMessage(client0.delegate(client0.generateUID("1.txt"), client1, 30, Permission.checkout, true));
+
+        System.out.print("Client1: delegating to client2...");
+        getSuccessMessage(client1.delegate(client1.generateUID("1.txt"), client2, 60, Permission.checkout, false));
+
+        System.out.print("Client2: checking out 1.txt...");
+        text = getSuccessMessage(client2.check_out(client2.generateUID("1.txt"))).getMessage();
+        System.out.println("message: " + text);
+        saveFile("client_2", "1_copy2.txt", text);
+
+        System.out.println("sleep 30 seconds...");
+        Thread.sleep(30000);
+        System.out.print("Client2: checking out 1.txt...");
+        getErrorMessage(client2.check_out(client2.generateUID("1.txt")));
+
+        System.out.println("sleep 60 seconds...");
+        Thread.sleep(60000);
+        System.out.print("Client2: checking out 1.txt...");
+        getErrorMessage(client2.check_out(client2.generateUID("1.txt")));
+
+        client0.terminate();
+        client1.terminate();
+        client2.terminate();
+        System.out.println("test complete!");
     }
 }
