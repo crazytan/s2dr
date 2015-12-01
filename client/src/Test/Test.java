14d4dd8e1564cce1d137c41ac9367d957edd94fd
crazytan <tjtanjia.tan@gmail.com>
2015-11-30 23:42:09 -0500

    resolve previous conflict

--- client/src/Test/Test.java
+++ client/src/Test/Test.java
@@ -3,31 +3,18 @@ package Test;
 import sun.security.rsa.RSAPublicKeyImpl;
 import org.bouncycastle.openssl.PEMParser;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileReader;
+import java.io.*;
 
 /**
  * Test script
  */
 public class Test {
 
-    private static String workspace;
+    private static final File workspace = new File(System.getenv("workspace"));
 
-    private static String root;
-
-    private static void initialize() throws Exception {
-        workspace = System.getenv("workspace");
-        root = System.getenv("root");
-
-        BufferedReader reader = new BufferedReader(new FileReader(root + "/CA"));
-        PEMParser parser = new PEMParser(reader);
-        Object key = parser.readObject();
-        System.out.println(key);
-    }
+    private static final File root = new File(System.getenv("root"));
 
     public static void main(String[] args) throws Exception {
-        initialize();
+
     }
 }
